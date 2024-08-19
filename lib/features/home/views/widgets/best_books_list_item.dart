250ab5a67cd0ad7f76d66384280dba01172202f3
ItsHLa <nissanihala@gmail.com>
2024-08-19 21:09:05 +0300

    adding best books list item details

--- lib/features/home/views/widgets/best_books_list_item.dart
+++ lib/features/home/views/widgets/best_books_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:folio/core/assets.dart';
+import 'package:folio/core/styles.dart';
 
 class BestBooksListItem extends StatelessWidget {
   const BestBooksListItem({super.key});
@@ -7,7 +8,7 @@ class BestBooksListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.only(left: 8.0 , right: 8 , bottom: 8),
       child: Row(
         children: [
           SizedBox(
@@ -23,6 +24,28 @@ class BestBooksListItem extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(width: 30,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width *.5,
+                child: const Text(
+                    'Harry Potter and The Goblet Of Fire',
+                  overflow:TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Styles.textStyle20,
+                ),
+              ),
+              const SizedBox(height: 3,),
+              const Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              Text('19.9',style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),),
+              const SizedBox(height: 3,),
+            ],
+          )
         ],
       ),
     );
--- lib/features/home/views/widgets/home_view_body.dart
+++ lib/features/home/views/widgets/home_view_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:folio/core/styles.dart';
-import 'package:folio/features/home/views/widgets/best_books_list_item.dart';
 import 'best_books_list_view.dart';
 import 'customized_app_bar.dart';
 import 'featured_book_list_view.dart';
@@ -12,13 +11,13 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const Padding(
-      padding: EdgeInsets.all(16.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomizeAppBar(),
           FeaturedBooksListView(),
-          SizedBox(height: 32,),
+          SizedBox(height: 20,),
           Text('Best Books' , style: Styles.textStyle18,),
           Expanded(child: BestBookListView())
         ],
